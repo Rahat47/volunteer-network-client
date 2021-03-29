f6d7f0d3b44d95d5dc47940d3536548ed9c9b4c3
Rahat <djrayhan8@gmail.com>
2021-03-30 00:45:13 +0600

    some home componenets

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,31 +5,44 @@ import { Container, Divider, Header, Input, Segment } from "semantic-ui-react";
 import "./home.css";
 const Home = () => {
     return (
-        <Container>
-            <Segment style={{ background: "transparent" }}>
-                <Navbar />
-            </Segment>
-            <Segment className="big-header" padded>
-                <Header textAlign="center" as="h1" size="huge">
-                    I grow by helping people in need.
-                </Header>
-                <Divider hidden />
-                <Input
-                    className="search-input"
-                    action={{
-                        color: "teal",
-                        labelPosition: "right",
-                        icon: "search",
-                        content: "Search",
-                    }}
-                    actionPosition="right"
-                    placeholder="Search For an Event...."
-                />
-            </Segment>
-            <Segment>
-                <EventContainer />
-            </Segment>
-        </Container>
+        <>
+            <Container fluid className="header-bg">
+                <Container>
+                    <Segment style={{ background: "transparent" }}>
+                        <Navbar />
+                    </Segment>
+                    <Segment className="big-header" padded>
+                        <Header
+                            textAlign="center"
+                            as="h1"
+                            style={{ fontSize: "3rem" }}
+                        >
+                            I grow by helping people in need.
+                        </Header>
+                        <Divider hidden />
+                        <Input
+                            className="search-input"
+                            size="big"
+                            action={{
+                                color: "teal",
+                                labelPosition: "right",
+                                icon: "search",
+                                content: "Search",
+                            }}
+                            actionPosition="right"
+                            placeholder="Search For an Event...."
+                        />
+
+                        <Divider hidden />
+                    </Segment>
+                </Container>
+            </Container>
+            <Container>
+                <Segment>
+                    <EventContainer />
+                </Segment>
+            </Container>
+        </>
     );
 };
 
